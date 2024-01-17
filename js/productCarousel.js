276152a8e7a64a41445521cb8450c91769090609
kim_kayoung <kykim.dev@gmail.com>
2024-01-17 17:47:50 +0900

    mend

---
+++ js/productCarousel.js
@@ -0,0 +1,9 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowkeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
