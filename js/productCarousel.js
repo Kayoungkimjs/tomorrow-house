bb05f878ff140a592c0104ecb6d839b539dfcde4
kim_kayoung <kykim.dev@gmail.com>
2024-01-25 16:27:52 +0900

    ✨ Handle variety of carousels and refactor code

--- js/productCarousel.js
+++
@@ -1,9 +0,0 @@
-const productCarousel = tns({
-  container: '.product-carousel .slider-list',
-  controls: false,
-  navContainer: '.product-carousel .thumbnail-list',
-  navAsThumbnails: true,
-  arrowkeys: true,
-  mouseDrag: true,
-  preventScrollOnTouch: true,
-})
